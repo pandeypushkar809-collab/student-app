2e9c08d4b65aa274a2f468dd3797380c81110d76
pandeypushkar809-collab <pandeypushkar809@gmail.com>
2026-07-17 09:12:07 +0000

    Implement Kubernetes Secret and update deployment

--- app.js
+++ app.js
@@ -4,11 +4,20 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 const APP_NAME = process.env.APP_NAME || "Student App Default";
+const DB_USER = process.env.DB_USER || "Not Found";
+const DB_PASSWORD = process.env.DB_PASSWORD || "Not Found"
 
 app.get("/", (req, res) => {
-    res.send(APP_NAME);
+    res.send(`
+Application : ${APP_NAME}
+
+Database User : ${DB_USER}
+
+Database Password : ${DB_PASSWORD}
+`);
 });
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+    console.log(`${APP_NAME} is running on port ${PORT}`);
 });
+
